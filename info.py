cd38e8d5b18c5ac8522bf35630e7649f437bda09
IMMORTAL-DEVELOPER <91306144+IMMORTAL-DEVELOPER@users.noreply.github.com>
2023-07-21 19:20:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,8 +59,8 @@ G_FILTER = is_enabled(environ.get("G_FILTER", "True"), True)
 BUTTON_LOCK = is_enabled(environ.get("BUTTON_LOCK", "True"), True)
 
 # url shortner
-SHORT_URL = environ.get("SHORT_URL")
-SHORT_API = environ.get("SHORT_API")
+SHORT_URL = environ.get("https://omegalinks.in")
+SHORT_API = environ.get("f41e13de473d2475c6ee95b00be3eb28ff53c6fa")
 
 # Others
 IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
